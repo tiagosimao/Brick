1abdcd4453791125d12e19575b3842ff42ad17e5
Tiago Simão <tiagosimao@gmail.com>
2012-04-10 11:19:19 +0100

    Fixed test resources location

--- src/test/java/com/irenical/brick/test/BundleTest.java
+++ src/test/java/com/irenical/brick/test/BundleTest.java
@@ -1,3 +1,19 @@
+/*
+	This file is part of Brick.
+
+    Brick is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Brick is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Brick.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.irenical.brick.test;
 
 import java.io.BufferedReader;
@@ -12,7 +28,7 @@ import com.irenical.brick.BundleInterface;
 public class BundleTest {
 	
 	public static String getFileContents(String fileName,boolean removeWhitespaces) throws IOException{
-		InputStream is = DOMBundleTest.class.getClassLoader().getResourceAsStream(DOMBundleTest.class.getPackage().getName().replaceAll("[.]", "/")+"/"+fileName);
+		InputStream is = DOMBundleTest.class.getClassLoader().getResourceAsStream(fileName);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 		StringBuilder sb = new StringBuilder();
 		String line = null;
